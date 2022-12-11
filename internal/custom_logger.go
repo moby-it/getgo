0d228b4a0f61617a218dc3d59c650f8428244960
George Spanos <gespa11019@gmail.com>
2022-12-11 23:39:01 +0200

    bumb up loger version

--- internal/custom_logger.go
+++ internal/custom_logger.go
@@ -15,6 +15,6 @@ func SetLogger(logsPath string) (*os.File, error) {
 		return nil, err
 	}
 	log.SetOutput(file)
-	log.Println("Service Started. v1.0.5")
+	log.Println("Service Started. v1.0.0")
 	return file, nil
 }
