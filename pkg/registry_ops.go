d3417d603e20f158dfb551f5acbf1af3b1453bb3
George Spanos <gespa11019@gmail.com>
2022-12-11 15:12:07 +0200

    recreating container after push

--- pkg/registry_ops.go
+++ pkg/registry_ops.go
@@ -7,14 +7,22 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 const stable = "stable"
+
+var cli *client.Client
+var ctx context.Context
 
 func HandleContainerPush(w http.ResponseWriter, r *http.Request) {
 	var res HookResponse
@@ -40,7 +48,7 @@ func HandleContainerPush(w http.ResponseWriter, r *http.Request) {
 	}
 	if res.PushData.Tag == stable {
 		log.Println("Tag pushed is stable. Starting build of", fmt.Sprintf("%v:%v", serviceName, stable))
-		err := updateRunningContainer(res, serviceName)
+		err := updateContainer(res, serviceName)
 		if err != nil {
 			message := "Failed to update container"
 			log.Println(message, serviceName, err)
@@ -54,12 +62,13 @@ func HandleContainerPush(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	defer r.Body.Close()
 }
-func updateRunningContainer(hookResponse HookResponse, serviceName string) error {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+func updateContainer(hookResponse HookResponse, serviceName string) error {
+	ctx = context.Background()
+	_cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		return err
 	}
+	cli = _cli
 	defer cli.Close()
 	if err != nil {
 		return err
@@ -68,29 +77,58 @@ func updateRunningContainer(hookResponse HookResponse, serviceName string) error
 	if err != nil {
 		return err
 	}
-	err = restartContainer(serviceName, cli, ctx)
+	container, err := destroyContainer(serviceName)
+	if err != nil {
+		return err
+	}
+	portMap, err := extractPortMapFromContainer(container)
+	if err != nil {
+		return err
+	}
+	err = recreateContainer(serviceName, hookResponse.Repository.RepoName, portMap)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 	return nil
 }
-func restartContainer(containerName string, cli *client.Client, ctx context.Context) error {
+func destroyContainer(containerName string) (*types.Container, error) {
 	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 	if err != nil {
-		return err
+		return nil, err
 	}
-	var container types.Container
 	if len(containers) == 0 {
-		return fmt.Errorf("no active containers")
+		return nil, fmt.Errorf("no active containers")
 	}
 	for _, c := range containers {
 		if strings.Contains(c.Names[0], containerName) {
-			container = c
-			break
+			stopTimeout := 2 * time.Minute
+			cli.ContainerStop(ctx, c.ID, &stopTimeout)
+			cli.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{})
+			return &c, nil
 		}
 	}
-	duration := 2 * time.Minute
-	cli.ContainerRestart(ctx, container.ID, &duration)
+	return nil, fmt.Errorf("failed to destroy container. No container found with name %v", containerName)
+
+}
+func recreateContainer(containerName string, imageName string, portMap nat.PortMap) error {
+	container, err := cli.ContainerCreate(ctx, &container.Config{Image: fmt.Sprintf("%v:stable", imageName)}, &container.HostConfig{PortBindings: portMap}, &network.NetworkingConfig{}, &v1.Platform{}, containerName)
+	if err != nil {
+		return err
+	}
+	err = cli.ContainerStart(ctx, container.ID, types.ContainerStartOptions{})
+	if err != nil {
+		return err
+	}
 	return nil
+}
+func extractPortMapFromContainer(container *types.Container) (nat.PortMap, error) {
+	portMap := make(nat.PortMap, 1)
+	if len(container.Ports) <= 0 {
+		return nil, fmt.Errorf("container has no open ports")
+	}
+	firstPort := container.Ports[0]
+	publicPort := strconv.Itoa(int(firstPort.PublicPort))
+	portMap[nat.Port(fmt.Sprintf("%v/%v", firstPort.PrivatePort, firstPort.Type))] = []nat.PortBinding{{HostIP: firstPort.IP, HostPort: publicPort}}
+	return portMap, nil
 }
